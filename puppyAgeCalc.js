c10a82ac6b8b6aaed10dc14114fa3eb26a2b116c
rchouhani <rchouhani@me.com>
2025-01-30 16:54:41 +0100

    exercice Bonus

--- puppyAgeCalc.js
+++ puppyAgeCalc.js
@@ -1,5 +1,6 @@
 const calculateDogAge = (puppysAge, humanAge) => {
-    console.log(`Your dog has ${puppysAge * 7} in Dog's years, which means ${humanAge / 7} in human age`)
+    
+    console.log(`Your dog has ${puppysAge * 7} in dog's years, which means ${humanAge / 7} in human age`)
 }
 calculateDogAge(5, 65)
 calculateDogAge(12, 56)
