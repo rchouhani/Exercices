05b55ffa974239c1b70322f45aadb70201e899e4
rchouhani <rchouhani@me.com>
2025-01-30 15:37:51 +0100

    exercices bonus

---
+++ exoFortuneTeller.js
@@ -0,0 +1,8 @@
+const tellFortune = (numberOfChildren, partnersName, geographicLocation, jobTitle) => {
+    console.log(`You will be a ${jobTitle} in ${geographicLocation}, and married to ${partnersName} with ${numberOfChildren} kids.`)
+  }
+
+tellFortune(4, 'Rosie', 'Roma', 'Programmer')
+tellFortune(12, 'Edouard', 'Lima', 'Scrum Master')
+tellFortune(1, 'Barbra', 'Madrid', 'Designer')
+tellFortune(2, 'Conchetta', 'Kerrouchen', 'Agricultrice')
---
+++ puppyAgeCalc.js
@@ -0,0 +1,6 @@
+const calculateDogAge = (puppysAge, humanAge) => {
+    console.log(`Your dog has ${puppysAge * 7} in Dog's years, which means ${humanAge / 7} in human age`)
+}
+calculateDogAge(5, 65)
+calculateDogAge(12, 56)
+calculateDogAge(8, 12)
