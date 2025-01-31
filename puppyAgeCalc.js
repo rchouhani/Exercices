3e37dd54b5926e4a03d9c34d97e002450c8f026a
rchouhani <rchouhani@me.com>
2025-01-31 09:55:20 +0100

    ajout d'une ligne de code pour arrondir l'age de l'humain

--- puppyAgeCalc.js
+++ puppyAgeCalc.js
@@ -1,6 +1,6 @@
 const calculateDogAge = (puppysAge, humanAge) => {
-    
-    console.log(`Your dog has ${puppysAge * 7} in dog's years, which means ${humanAge / 7} in human age`)
+    let c = parseInt(humanAge / 7) // permet d'arrondir à l'unité
+    console.log(`Your dog has ${puppysAge * 7} in dog's years, which means ${c} in human age`)
 }
 calculateDogAge(5, 65)
 calculateDogAge(12, 56)
