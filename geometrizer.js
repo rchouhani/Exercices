6cf9f4690b4ef089d4c2b8186910ec8716e1aed4
rchouhani <rchouhani@me.com>
2025-01-31 16:10:05 +0100

    Menage dans le rangement des exercices

---
+++ geometrizer.js
@@ -0,0 +1,11 @@
+const calcCircumfrence = (rayon) => {
+    console.log(2 * 3.14 * rayon)
+    return calcCircumfrence
+}
+calcCircumfrence(10)
+
+
+const calcArea = (calcCircumfrence) => {
+    console.log(3.14 * calcCircumfrence**2)
+}
+calcArea(2)
