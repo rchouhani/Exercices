7309910c3fe80de3ff6f1d9ba0fbd6d7aaab63f1
rchouhani <rchouhani@me.com>
2025-04-04 15:56:23 +0200

    04042025

---
+++ sqlExo/candidates.js
@@ -0,0 +1,5 @@
+const candidates = ['Le Filip','Ruby On The Nail','Leona Winter','Lula Strega','Misty Phoenix',
+    'Perseo','Norma Bell','Edeha Noire','Magnetica','Afrodite Amour'];
+for (let i = 0; i < candidates.length; i++){
+console.log(candidates[i])
+}
