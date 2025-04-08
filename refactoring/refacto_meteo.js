00f79bc297068ab6231126c3e094a76d68290606
rchouhani <rchouhani@me.com>
2025-04-08 17:12:11 +0200

    ajouts d'exos de refactoring

---
+++ refactoring/refacto_meteo.js
@@ -0,0 +1,46 @@
+const getRainSpan = document.querySelector('#pluie');
+const getDegreesSpan = document.querySelector('#degres');
+const getCitySpan = document.querySelector('#ville');
+
+const fetchWeather = async () => {
+    let resp = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${48.8534}&longitude=${2.3488}&current=temperature_2m,rain`);
+    let json = await resp.json();
+    return json;
+}
+
+const displayWeather = async (json) => {
+      console.log("😋😋");
+}
+displayWeather()
+    // console.log(json.current.rain)
+    // document.getElementById("pluie_paris").innerHTML = "<strong>" + json.current.rain + "</strong>"
+    // document.getElementById("degres_paris").innerHTML = "<strong>" + json.current.temperature_2m + "</strong>"
+  
+    // console.log(json.current.rain)
+    // document.getElementById("pluie_lyon").innerHTML = "<strong>" + json.current.rain + "</strong>"
+    // document.getElementById("degres_lyon").innerHTML = "<strong>" + json.current.temperature_2m + "</strong>"
+    
+    // console.log(json.current.rain)
+    // document.getElementById("pluie_nantes").innerHTML = "<strong>" + json.current.rain + "</strong>"
+    // document.getElementById("degres_nantes").innerHTML = "<strong>" + json.current.temperature_2m + "</strong>"
+    
+    // console.log(json.current.rain)
+    // document.getElementById("pluie_marseille").innerHTML = "<strong>" + json.current.rain + "</strong>"
+    // document.getElementById("degres_marseille").innerHTML = "<strong>" + json.current.temperature_2m + "</strong>"
+    
+    // console.log(json.current.temperature_2m)
+    // document.getElementById("pluie_lille").innerHTML = "<strong>" + json.current.rain + "</strong>"
+    // document.getElementById("degres_lille").innerHTML = "<strong>" + json.current.temperature_2m + "</strong>"
+
+  
+  fetchWeather();
+    // const LAT_PARIS = 48.8534;
+    // const LONG_PARIS = 2.3488;
+    // const LAT_NANTES = 47.2173;
+    // const LONG_NANTES = -1.5534;
+    // const LAT_LYON = 45.75;
+    // const LONG_LYON = 4.85;
+    // const LAT_MARSEILLE = 43.3;
+    // const LONG_MARSEILLE = 5.4;
+    // const LAT_LILLE = 50.633333;
+    // const LONG_LILLE = 3.066667;
