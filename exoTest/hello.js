cbe230ba7d4b3f167d55588b7220fbe5d23d105b
rchouhani <rchouhani@me.com>
2025-04-09 15:14:03 +0200

    rangement des exercices individuels

---
+++ exoTest/hello.js
@@ -0,0 +1,5 @@
+const sayHello = (name) => {
+    return `Bonjour, ${name} !`;
+}
+
+module.exports = { sayHello }
--- refactoring/refacto_meteo.js
+++ refactoring/refacto_meteo.js
@@ -3,15 +3,19 @@ const getDegreesSpan = document.querySelector('#degres');
 const getCitySpan = document.querySelector('#ville');
 
 const fetchWeather = async () => {
-    let resp = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${48.8534}&longitude=${2.3488}&current=temperature_2m,rain`);
-    let json = await resp.json();
-    return json;
+  console.log("🙄🙄", json);
+    let response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${48.8534}&longitude=${2.3488}&current=temperature_2m,rain`);
+    let data = await response.json();
+    return data;
 }
+fetchWeather();
 
-const displayWeather = async (json) => {
-      console.log("😋😋");
+
+const displayWeather = async (data) => {
+      console.log("😋😋", data);
 }
-displayWeather()
+displayWeather();
+
     // console.log(json.current.rain)
     // document.getElementById("pluie_paris").innerHTML = "<strong>" + json.current.rain + "</strong>"
     // document.getElementById("degres_paris").innerHTML = "<strong>" + json.current.temperature_2m + "</strong>"
@@ -33,7 +37,6 @@ displayWeather()
     // document.getElementById("degres_lille").innerHTML = "<strong>" + json.current.temperature_2m + "</strong>"
 
   
-  fetchWeather();
     // const LAT_PARIS = 48.8534;
     // const LONG_PARIS = 2.3488;
     // const LAT_NANTES = 47.2173;
