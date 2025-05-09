b736c6e39c15f0ac0448dd69e14c4f39c840b7dc
rchouhani <rchouhani@me.com>
2025-05-09 18:44:23 +0200

    Script sur la récursivité fonctionne

--- exoRecursivite/script.js
+++ exoRecursivite/script.js
@@ -57,41 +57,46 @@ const displayFolder = (zucchini) => {
     console.log('📁', zucchini.nom);
 }
 
-const displayIterativeFolder = (param) => {
-    displayFolder(param);
-  for (let i = 0; i < param.contenu.length; i++){
-
-    if(param.contenu[i].contenu){
-      console.log('📁', param.contenu[i].nom);
-      for (let j = 0; j < param.contenu[i].contenu.length; j++){
-        // console.log(param.contenu[i].contenu[j].nom)
-        if(param.contenu[i].contenu[j]){
-          console.log('📁', param.contenu[i].contenu[j].nom)
-        }
-      }
-
-    } else {
-      console.log('📑', param.contenu[i].nom)
-    }
-  }
-}
-displayIterativeFolder(dossierPrincipal);
-// console.log(dossierPrincipal.contenu[1].nom);
-console.log(dossierPrincipal.contenu);
-// console.log('⚽', dossierPrincipal.contenu[2].contenu[0]);
-
-
-// const displayRecursiveFolder = (param) => {
+// const displayIterativeFolder = (param) => {
+//     displayFolder(param);
 //   for (let i = 0; i < param.contenu.length; i++){
 
-//     if(param.contenu[i].contenu !== undefined){
+//     if(param.contenu[i].contenu){
 //       console.log('📁', param.contenu[i].nom);
+//       for (let j = 0; j < param.contenu[i].contenu.length; j++){
+//           if(param.contenu[i].contenu[j]){
+//             console.log('📁', param.contenu[i].contenu[j].nom)
+//           }
+      
+//         for(let k = 0; k < param.contenu[i].contenu[j].contenu.length; k++){
+//             if(param.contenu[i].contenu[j].contenu[k]){
+//               console.log('📑', param.contenu[i].contenu[j].contenu[k].nom);
+//             }
+//         } 
+//       }
+//     } else {
+//       console.log('📑', param.contenu[i].nom)
 //     }
-     
-//     if(param.contenu[i].contenu === undefined){
-//       console.log('📑', param.contenu[i].nom);
-//     }
-
 //   }
 // }
-// displayRecursiveFolder(dossierPrincipal);
+// displayIterativeFolder(dossierPrincipal);
+
+
+
+const displayRecursiveFolder = (param) => {
+  displayRecursiveFolder()
+  // console.log(param.nom)
+
+  // for (let i = 0; i < param.contenu.length; i++){
+
+  //   if(param.contenu[i].contenu !== undefined){
+  //     console.log('📁', param.contenu[i].nom);
+  //   }
+     
+  //   if(param.contenu[i].contenu === false){
+  //     console.log('📑', param.contenu[i].nom);
+  //   }
+
+  }
+// }
+displayRecursiveFolder(dossierPrincipal);
